fc7bf2d2f1a2799f62971645f66306c28e46396a
vipulhegdeV <109965507+vipulhegdeV@users.noreply.github.com>
2022-07-25 08:32:57 -0700

    updated pythondoubleovertake

--- DOUBLEOVERTAKE.py
+++ DOUBLEOVERTAKE.py
@@ -1,19 +1,26 @@
 import os
 import time
-#setting up paramters
+#setting up parameters
+
 strech=int(input("Enter the STRECTH length  MAX= 30KM :\t"))
 premdis=int(input("Enter the premium train distance from S end:\t"))
 premspeed=int(input("Enter the MPS of premium train in kmph: "))
 leaddis=int(input("Enter the lead train distance from S end:\t"))
 next_statdis=int(input("Enter the distance from lead train to next first looping station available:\t"))
+
 #computations
+
 reldist=0
 reldist=premdis-leaddis
 print("DISTANCE OF PREMIUM TRAIN TO LEAD TRAIN IS (IN KM)=",reldist)
+
+#Decisionmaking
+
 if reldist<=30 and reldist>=10:   #IF RELATIVE TRAIN DISTANCE IS MORE THAN 10KM AND LESS THAN 30 KM OR MORE THAN 30 KM FLOW
     if next_statdis<=10:
         response=input("IS LOOP LINE ONE FREE Y =YES \nN=SECONDLINE, \nNO LOOP LINE FREE ENTER ZERO : ")
         response.upper()
+        
         if response=='Y':
           print("LEAD TRAIN MOVE TO LOOP LINE ONE, SET SIGNAL L1 : RED\nPREMIUM TRAIN PROCEED AT MPS TO NEXT STRECH, SET SIGNAL ML: GREEN")
           overtake_time=0.0
@@ -22,6 +29,7 @@ if reldist<=30 and reldist>=10:   #IF RELATIVE TRAIN DISTANCE IS MORE THAN 10KM 
           signal_time=0.0
           signal_time=overtake_time+0.02
           print("SIGNAL L1: GREEN IN hrs ",signal_time)
+        
         elif response=='N':
             print("LEAD TRAIN MOVE TO LOOP LINE TWO, SET SIGNAL L2 : RED\n SIGNAL L1: RED \n PREMIUM TRAIN PROCEED AT MPS TO NEXT STRECH, SET SIGNAL ML: GREEN")
             overtake_time=reldist/premspeed
@@ -35,7 +43,9 @@ if reldist<=30 and reldist>=10:   #IF RELATIVE TRAIN DISTANCE IS MORE THAN 10KM 
             print("invalid response enter only options mentioned above")   
     else: 
         print("OVERTAKE  SET TO  NEXT LOOPING STATION  ALL SIGNAL: GREEN") 
+        
 elif reldist<=10:                  #FOR RELATIVE  DISTANCE IS LESS THAN 10 KM FLOW
+    
   lspeed=int(input("Enter the Lead train speed in kmph: "))
   suc_statdis=int(input("Enter the distance from lead train to next second sucessive looping station available: "))
   premsetsp_time=(suc_statdis/lspeed)+0.02
@@ -48,3 +58,5 @@ elif reldist<=10:                  #FOR RELATIVE  DISTANCE IS LESS THAN 10 KM FL
   print("SIGNAL L2: GREEN IN ",signal_time)
 else:
     print("OVERTAKE NOT POSSIBLE IN THIS STRECTH, BOTH TRAINS PROCEED AS USUAL")
+    
+    #endofprogram
